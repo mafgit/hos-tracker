168839d132e9df60208e2229033b8c677f10ee66
Abdullah <mohammadabdullahfarooqui27@gmail.com>
2026-07-23 22:18:07 +0500

    deployment fix

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -39,7 +39,28 @@ ALLOWED_HOSTS = ["localhost", "127.0.0.1", ".vercel.app", "hos-tracker-backend.v
 #]
 
 CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = False
 
+CORS_ALLOW_METHODS = [
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
+]
+
+CORS_ALLOW_HEADERS = [
+    "accept",
+    "accept-encoding",
+    "authorization",
+    "content-type",
+    "dnt",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+]
 
 CSRF_TRUSTED_ORIGINS = [
 "https://hos-tracker.vercel.app",
