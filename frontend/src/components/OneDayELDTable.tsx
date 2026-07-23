e2f080457e5edb8b643ccf4a8bc53839fa577a65
Abdullah <mafishere@gmail.com>
2026-07-23 05:20:39 +0500

    added sum in svg

--- frontend/src/components/OneDayELDTable.tsx
+++ frontend/src/components/OneDayELDTable.tsx
@@ -3,19 +3,25 @@ import type { DayLog } from "../types/SimulationDataType";
 const arr25 = Array.from({ length: 25 });
 const arr2d = Array.from({ length: 4 }).fill(Array.from({ length: 24 }));
 
-const startX = 40;
+const startX = 60;
 const boxWidth = 40;
 const startY = 40;
-const endX = 1000;
+const endX = startX + boxWidth * 24;
 const endY = 200;
 
 function timeToPixels(time: number): number {
   return Math.floor((time / 25) * 1000) + startX;
 }
 
-function logsToPathString(oneDayLogs: DayLog[]): string {
-  console.log("oneDayLogs", oneDayLogs);
+function processDayLogs(oneDayLogs: DayLog[]) {
   const path: string[] = ["M"];
+  const sums: Record<string, any> = {
+    ON_DUTY_NOT_DRIVING: 0,
+    SLEEPER_BERTH: 0,
+    DRIVING: 0,
+    OFF_DUTY: 0,
+  };
+
   oneDayLogs.forEach((log, i) => {
     const y =
       log.state === "OFF_DUTY"
@@ -31,12 +37,26 @@ function logsToPathString(oneDayLogs: DayLog[]): string {
     const from = `${x1} ${y}`;
     const to = `${x2} ${y}`;
     path.push(from, to);
+
+    // sum
+    sums[log.state as keyof typeof sums] += log.to - log.from;
   });
 
-  if (oneDayLogs.length === 1) return "";
-  const str = path.join(" ");
-  console.log(str);
-  return str;
+  const pathString = oneDayLogs.length > 1 ? path.join(" ") : "";
+
+  for (const key in sums) {
+    const a = sums[key as keyof typeof sums] as number;
+    const hrs = Math.floor(a);
+    const mins = Math.round((a - hrs) * 60);
+    const hrsStr = hrs < 10 ? `0${hrs}` : `${hrs}`;
+    const minsStr = mins < 10 ? `0${mins}` : `${mins}`;
+    sums[key as keyof typeof sums] = `${hrsStr} ${minsStr}`;
+  }
+
+  return {
+    pathString,
+    sums,
+  };
 }
 
 export default function OneDayELDTable({
@@ -44,11 +64,12 @@ export default function OneDayELDTable({
 }: {
   oneDayLogs: DayLog[];
 }) {
+  const { pathString, sums } = processDayLogs(oneDayLogs);
   return (
     <div className="flex gap-1">
       {/*  */}
       <div className="bg-linear-30 from-[#ffe75e] to-[#a0ffff] w-full p-2 rounded-lg">
-        <svg viewBox="0 0 1040 200" className="w-full h-[200px]">
+        <svg viewBox="0 0 1080 200" className="w-full h-[200px]">
           <g className="stroke-bg">
             {/* horizontal lines */}
             <line x1={startX} y1={startY} x2={endX} y2={startY} />
@@ -77,27 +98,63 @@ export default function OneDayELDTable({
               y2={startY + boxWidth * 4}
             />
 
-            <text x={0} y={60} fontSize="10" fill="#000000" fontWeight={"100"}>
+            <text x={startX - 50} y={65} fontSize="10" fill="#000000" fontWeight={"100"}>
               On Duty
             </text>
+            <text
+              x={endX + 10}
+              y={65}
+              fontSize="12"
+              fill="#000000"
+              fontWeight={"100"}
+            >
+              {sums.ON_DUTY_NOT_DRIVING}
+            </text>
 
-            <text x={0} y={100} fontSize="10" fill="#000000" fontWeight={"100"}>
+            <text x={startX - 50} y={105} fontSize="10" fill="#000000" fontWeight={"100"}>
               S. Berth
             </text>
+            <text
+              x={endX + 10}
+              y={105}
+              fontSize="12"
+              fill="#000000"
+              fontWeight={"100"}
+            >
+              {sums.SLEEPER_BERTH}
+            </text>
 
-            <text x={0} y={140} fontSize="10" fill="#000000" fontWeight={"100"}>
+            <text x={startX - 50} y={145} fontSize="10" fill="#000000" fontWeight={"100"}>
               Driving
             </text>
-
-            <text x={0} y={180} fontSize="10" fill="#000000" fontWeight={"100"}>
-              Off Duty
+            <text
+              x={endX + 10}
+              y={145}
+              fontSize="12"
+              fill="#000000"
+              fontWeight={"100"}
+            >
+              {sums.DRIVING}
             </text>
 
-            {/* vertical lines */}
+            <text x={startX - 50} y={185} fontSize="10" fill="#000000" fontWeight={"100"}>
+              Off Duty
+            </text>
+            <text
+              x={endX + 10}
+              y={185}
+              fontSize="12"
+              fill="#000000"
+              fontWeight={"100"}
+            >
+              {sums.OFF_DUTY}
+            </text>
+
+            {/* times at top */}
             {arr25.map((_, i) => (
               <g key={"v-" + i}>
                 <text
-                  x={(i + 1) * 40}
+                  x={startX + i * 40}
                   y={20}
                   fontSize="12"
                   fill="#000000"
@@ -105,14 +162,20 @@ export default function OneDayELDTable({
                 >
                   {i === 0 || i === 24 ? "M" : i === 12 ? "N" : i}
                 </text>
-                <line x1={(i + 1) * 40} y1={40} x2={(i + 1) * 40} y2={200} />
+                {/* vertical lines */}
+                <line
+                  x1={startX + i * 40}
+                  y1={40}
+                  x2={startX + i * 40}
+                  y2={200}
+                />
               </g>
             ))}
 
             {/* inner 15 minute interval lines */}
             {arr2d.map((row, y) =>
               row.map((_, x) => {
-                const thisX = (x + 1) * 40;
+                const thisX = startX + x * 40;
                 const thisY = (y + 1) * 40;
                 return (
                   <g key={"inner-" + x + "-" + y}>
@@ -141,7 +204,7 @@ export default function OneDayELDTable({
 
             {oneDayLogs.length > 0 && (
               <path
-                d={logsToPathString(oneDayLogs)}
+                d={pathString}
                 fill="none"
                 stroke="red"
                 strokeWidth="3"
